9814db7c131e096d5ca6effd2da50534f983ee79
Pablo Oliveira <pablogui1611@gmail.com>
2021-03-21 11:30:01 -0300

    Produto e associacao N para N

--- src/main/java/com/avantetech/cursomc/domain/Categoria.java
+++ src/main/java/com/avantetech/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.avantetech.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -18,6 +21,11 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	
+	@ManyToMany(mappedBy = "categorias") // eu estou referenciado o mapeando do outro lado ou seja de Produtos.
+	private List<Produto> produtos = new ArrayList<>();
+	
 	
 	public Categoria() {
 		// TODO Auto-generated constructor stub
@@ -43,6 +51,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -74,6 +90,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
---
+++ src/main/java/com/avantetech/cursomc/domain/Produto.java
@@ -0,0 +1,120 @@
+package com.avantetech.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Produto implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	private Double preco;
+	
+	@ManyToMany //muitos para muitos
+	@JoinTable(name="PRODUTO_CATEGORIA",
+	joinColumns = @JoinColumn(name= "produto_id"),
+	inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	) 
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	
+//	Quando se tem N:N uma terceira table é gerada, então com os paramentro acima informo:
+//	@JoinTable(name="PRODUTO_CATEGORIA" nome da tabela gerada;
+//	joinColumns = @JoinColumn(name= "produto_id") nome do campo tabela correspondete ao codigo do produto ou seja
+//			chave estrangeira
+//	inverseJoinColumns = @JoinColumn(name = "categoria_id")=> outra chave estrangeira que referencia a categoria
+
+	
+	
+	public Produto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", preco=" + preco + ", categorias=" + categorias + "]";
+	}
+	
+	
+
+}
