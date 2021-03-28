d58eb4cd194366d63c355abcefc39bc94ee518fc
Pablo Oliveira <pablogui1611@gmail.com>
2021-03-27 23:53:50 -0300

    Personalizando retorno da validacao

---
+++ src/main/java/com/avantetech/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,38 @@
+package com.avantetech.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	private List<FieldMessage> list = new ArrayList<>();
+	
+	
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addError(String fieldName, String messagem) {
+		list.add(new FieldMessage(fieldName, messagem));
+	}
+
+	public List<FieldMessage> getErrors() {
+		return list;
+	}
+
+	public void setList(List<FieldMessage> list) {
+		this.list = list;
+	}
+	
+	
+
+}
