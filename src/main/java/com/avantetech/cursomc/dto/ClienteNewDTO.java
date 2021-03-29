adce9d4a7c7f102b5b155ab1e441af3a5157cd25
Pablo Oliveira <pablogui1611@gmail.com>
2021-03-28 22:12:19 -0300

    Validacao customizada de cpf ou cpnj, na insercao de cliente

--- src/main/java/com/avantetech/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/avantetech/cursomc/dto/ClienteNewDTO.java
@@ -2,6 +2,14 @@ package com.avantetech.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.avantetech.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 
 	
@@ -9,18 +17,37 @@ public class ClienteNewDTO implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Length(min = 5, message = "O Tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
+	@Email
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String cpfOuCnpj;
+	
+	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String logradouro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String cep;
 	
+	@NotEmpty(message = "Preenchimento Obrigatorio")
 	private String telefone1;
+	
+	
 	private String telefone2;
 	private String telefone3;
 	
