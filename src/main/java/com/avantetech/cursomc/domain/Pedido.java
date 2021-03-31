6c466ce5d01ea78fb7eb1156a6f2565ee59cf44c
Pablo Oliveira <pablogui1611@gmail.com>
2021-03-31 07:50:59 -0300

    Implementando toString de  pedido

--- src/main/java/com/avantetech/cursomc/domain/Pedido.java
+++ src/main/java/com/avantetech/cursomc/domain/Pedido.java
@@ -1,8 +1,12 @@
 package com.avantetech.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -118,11 +122,6 @@ public class Pedido implements Serializable {
 		this.itens = itens;
 	}
 
-	@Override
-	public String toString() {
-		return "Pedido [id=" + id + ", instante=" + instante + ", pagamento=" + pagamento + ", cliente=" + cliente
-				+ ", enderecoDeEntrega=" + enderecoDeEntrega + "]";
-	}
 
 	@Override
 	public int hashCode() {
@@ -148,6 +147,30 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes: \n");
+		for(ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
+	}
+	
+	
 
 
 
